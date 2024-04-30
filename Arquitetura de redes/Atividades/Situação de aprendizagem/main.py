37ba7fd4c273adc9b7bf64fa3da182a2101039ca
Maria Eduarda <165069113+MariaGomesR@users.noreply.github.com>
2024-04-30 14:32:54 -0300

    Add files via upload

---
+++ Arquitetura de redes/Atividades/Situação de aprendizagem/main.py
@@ -0,0 +1,22 @@
+import machine
+import time
+ 
+led_amarelo = machine.Pin(26, machine.Pin.OUT)
+led_vermelho = machine.Pin(14, machine.Pin.OUT)
+led_verde = machine.Pin(27, machine.Pin.OUT)
+
+while True:
+  led_verde.value(1)
+  time.sleep(3)
+  led_verde.value(0)
+
+  led_amarelo.value(1)
+  time.sleep(1)
+  led_amarelo.value(0)
+  
+  led_vermelho.value(1)
+  time.sleep (5)
+  led_vermelho.value(0)
+ 
+
+       
